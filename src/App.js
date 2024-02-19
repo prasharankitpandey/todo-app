bb421c9ab67eb3f311b9136c9cfa6d053288a700
prasharankitpandey <prasharankitpandey@gmail.com>
2024-02-19 21:34:00 +0530

    added form handling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [taskName, setTaskName] = useState('');
+
+  function add(taskName) {
+    setTodoList([...todoList, taskName])
+    setTaskName('')
+  }
+
+  function remove(id) {
+    const newList = todoList.filter((item, index) => id !== index);
+    setTodoList(newList)
+  }
+
   return (
     <div className='App'>
+      <div className='title'>Todo List</div>
       <div className='container'>
-        hello world
+        <div className='listinput'>
+          <div id='att'>Task Name :</div>
+          <input type='text' id='name' placeholder='Add your task here.' value={taskName} onChange={(e) => setTaskName(e.target.value)}></input>
+          <button className='btn' disabled={!taskName.length} onClick={() => add(taskName)}>Add Task</button>
+        </div>
+        <div className='TodoList'>
+          {todoList.map((task, index) => {
+            return (<div className='listitem' key={index}>
+              <div className='taskname'>{index+1}. {task}</div>
+              <button className='btn' onClick={() => remove(index)}>Remove</button>
+            </div>)
+          })}
+        </div>
       </div>
     </div>
+
   );
 }
+
 
 export default App;
